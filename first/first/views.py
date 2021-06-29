eaf0bf0627f215ce52958edff1a55066f1973c7b
SujanSharma07 <sujansharma202@gmail.com>
2021-06-29 10:12:24 +0545

    Template inheriting

--- first/first/views.py
+++ first/first/views.py
@@ -3,28 +3,35 @@ from django.shortcuts import render
 
 
 def home(request):
+    print("Home is triggred")
     try:
         data = request.GET
         maths = int(data['maths'])
         social = int(data['social'])
         total = maths + social
+        """ 
+        pickle_in = open('first/heart.pickle', 'rb')
+        reg = pickle.load(pickle_in)
+        input_ = np.array([i for i in range(13)]).reshape(-1, 1)
+        print(reg.predict(input_)) """
+
         percentage = total/2
         context = {'t': total, 'per': percentage,
                    'marks': [("Maths", 34), ("Social", 78), ("Science", 34)]}
         return render(request, 'output.html', context)
-    except:
-        pass
+    except Exception as e:
+        print(e)
 
     return render(request, 'home.html')
 
 
 def about(request):
-    print("about is called")
-    return render(request, 'home.html')
+    return render(request, 'about.html')
 
 
 def contact(request):
-    return HttpResponse('Welcome to Contact Page ')
+
+    return render(request, 'contact.html')
 
 
 def dynamic(request, roll):
